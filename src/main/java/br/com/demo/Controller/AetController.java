69df5c411f5a6dc9fb383589fcafb97cca2a03e7
joaoPedroDaCunha <joaopedrodacunha.santos@gmail.com>
2024-11-16 11:34:37 -0300

    Method get,post

---
+++ src/main/java/br/com/demo/Controller/AetController.java
@@ -0,0 +1,7 @@
+package br.com.demo.Controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AetController {
+}
--- src/main/java/br/com/demo/Controller/VehicleController.java
+++ src/main/java/br/com/demo/Controller/VehicleController.java
@@ -1,8 +1,35 @@
 package br.com.demo.Controller;
 
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.demo.Model.Vehicle;
+import br.com.demo.Repository.VehicleRepository;
 
 @RestController
 public class VehicleController {
-    
+
+    private @Autowired VehicleRepository vehicleRepository;
+    @GetMapping("/getvehicles")
+    public List<Vehicle> getVehicles(){
+        return vehicleRepository.findAll();    
+    }
+    @PostMapping("/postVehicle")
+    public void postVehicle(@RequestBody Vehicle vehicle){
+        vehicleRepository.save(vehicle);
+    }
+
+    @GetMapping("/vehicle/{plate}")
+    public Optional<Vehicle> getVehicleByPlate(@PathVariable("plate") String plate){
+        return vehicleRepository.findById(plate);
+    }
+
 }
