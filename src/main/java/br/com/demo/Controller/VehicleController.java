b7534918d973a531702d8fc6ace64d436d816d16
JoaoPedroDaCunha <joaopedrodacunha.santos@gmail.com>
2024-11-19 08:50:23 -0300

    create crud method vehicle

--- src/main/java/br/com/demo/Controller/VehicleController.java
+++ src/main/java/br/com/demo/Controller/VehicleController.java
@@ -4,9 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,6 +31,17 @@ public class VehicleController {
     @GetMapping("/vehicle/{plate}")
     public Optional<Vehicle> getVehicleByPlate(@PathVariable("plate") String plate){
         return vehicleRepository.findById(plate);
+    }
+
+    @PutMapping("/vehicle/atualizar")
+    public void putVehicle(@RequestBody Vehicle vehicle){
+        vehicleRepository.deleteById(vehicle.getPlate());
+        vehicleRepository.save(vehicle);
+    }
+
+    @DeleteMapping("/deleteVehicle/{plate}")
+    public void deleteById(@PathVariable String plate){
+        vehicleRepository.deleteById(plate);
     }
 
 }
